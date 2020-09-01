03321aeee854dcdaab3480f106a028aec351c9bd
alkhaldieid <eid.alkhaldi@gmail.com>
2020-08-31 22:27:25 -0400

    interface tweaks + yasnippet (empty) + evil mode stuff + wm in emacs + evil leader keybindgs + theme and extra packages from the french guy, org-roam not working yet'

---
+++ elpa/treemacs-evil-20200716.2041/treemacs-evil-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-evil" "20200716.2041" "Evil mode integration for treemacs" '((emacs "25") (evil "1.2.12") (treemacs "0.0")) :commit "a3562d8e63105a7b33fdac54c276f29b62c8f528" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
