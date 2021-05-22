9954f8a5cfee98fd6beeea62c46f89bb06c92f4c
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2021-05-22 13:05:11 -0400

    Sat May 22 01:05:11 PM EDT 2021

--- elpa/helm-bibtex-20200429.1606/helm-bibtex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-bibtex" "20200429.1606" "A bibliography manager based on Helm" '((bibtex-completion "1.0.0") (helm "1.5.5") (cl-lib "0.5") (emacs "24.1")) :commit "3f7bf30553e8161c7020b6aa92ad92933e00bafc" :authors '(("Titus von der Malsburg" . "malsburg@posteo.de")) :maintainer '("Titus von der Malsburg" . "malsburg@posteo.de") :url "https://github.com/tmalsburg/helm-bibtex")
