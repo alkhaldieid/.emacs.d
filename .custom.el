55d09ba9dd6fde023106fb72443a2b10b171d97d
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2022-03-24 14:18:49 -0400

    org-ref and note-taking and org-latex export working

---
+++ .custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(evil-undo-system 'undo-tree)
+ '(global-company-mode t)
+ '(org-agenda-files '("~/repos/org/agenda/monthly.org"))
+ '(package-selected-packages
+   '(openwith olivetti org-superstar yasnippet try mw-thesaurus ivy-bibtex org-ref hydra avy htmlize citeproc evil-org bibtex-completion org-roam which-key use-package undo-tree eyebrowse evil-leader counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ elpa/org-ref-20220318.2105/org-ref-pkg.el
@@ -0,0 +1,21 @@
+(define-package "org-ref" "20220318.2105" "citations, cross-references and bibliographies in org-mode"
+  '((org "9.4")
+    (dash "0")
+    (s "0")
+    (f "0")
+    (htmlize "0")
+    (hydra "0")
+    (avy "0")
+    (parsebib "0")
+    (bibtex-completion "0")
+    (citeproc "0"))
+  :commit "ec449195438f815698241f0b01a5395221b3306e" :authors
+  '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainer
+  '("John Kitchin" . "jkitchin@andrew.cmu.edu")
+  :keywords
+  '("org-mode" "cite" "ref" "label")
+  :url "https://github.com/jkitchin/org-ref")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
