1490b07865fb74feae0513a0cd7ece4afa41e07a
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2022-08-05 08:17:40 -0400

    pushed from laptop in Fri Aug  5 08:17:40 AM EDT 2022

--- .custom.el
+++ .custom.el
@@ -21,7 +21,7 @@
  '(ivy-posframe-mode t nil (ivy-posframe))
  '(ivy-posframe-width 120)
  '(org-agenda-files
-   '("/data/home/alkhaldieid/repos/org/roam/daily/2022-07-20.org" "/data/home/alkhaldieid/repos/org/agenda/salah.org" "/home/alkhaldieid/repos/csa/paper/paper.org" "/data/home/alkhaldieid/repos/csa/paper/paper.org" "/home/alkhaldieid/repos/org/agenda/monthly.org"))
+   '("/home/alkhaldieid/repos/csa/paper/paper.org" "/home/alkhaldieid/repos/org/agenda/monthly.org"))
  '(package-selected-packages
    '(ivy-emms command-log-mode evil-surround twittering-mode visual-fill-column academic-phrases ivy-pass ytdl org-present evil-collections ivy-posframe git-gutter git-gutter-+ git-gutter-plus evil-nerd-commenter company-statistics company-quickhelp rainbow-mode evil-collection projectile hc-zenburn-theme danneskjold-theme zenburn-theme leuven-theme ample-theme conda flyspell-correct-ivy elpy lsp-mode jupyter major-mode-hydra pyvenv ob-ipython org-roam-bibtex deft company marginalia magit openwith olivetti org-superstar yasnippet try mw-thesaurus ivy-bibtex org-ref hydra avy htmlize citeproc evil-org bibtex-completion org-roam which-key use-package undo-tree eyebrowse evil-leader counsel))
  '(smtpmail-smtp-server "email.utoledo.edu")
