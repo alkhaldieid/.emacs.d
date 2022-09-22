930ff2a55affeb0a21ce9585141c83a085245978
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2022-09-22 15:31:23 -0400

    begining of Fall 2022 semester

--- .custom.el
+++ .custom.el
@@ -8,22 +8,24 @@
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
  '(custom-safe-themes
-   '("a37d20710ab581792b7c9f8a075fcbb775d4ffa6c8bce9137c84951b1b453016" "738c4838957c1884dfacbb6f4f783c54e87c4a6b31c336d6279fc1c2b2ee56c5" "2eb6dd78a83cdecc2ea783a2640f4c316f41e223353d4a32fe936113e8a2ddbf" "9040edb21d65cef8a4a4763944304c1a6655e85aabb6e164db6d5ba4fc494a04" "0ed3d96a506b89c1029a1ed904b11b5adcebeb2e0c16098c99c0ad95cb124729" "b6c43bb2aea78890cf6bd4a970e6e0277d2daf0075272817ea8bb53f9c6a7f0a" "aba75724c5d4d0ec0de949694bce5ce6416c132bb031d4e7ac1c4f2dbdd3d580" "67f0f440afa2e68d9d00219b5a56308761af45832fb60769d2b2fd36e3fead45" default))
+   '("92cfd42cedb42fdd3ea0d84d518825a94f29b30be20f65978dab7d7c8fa30c6a" "0f964c8dbc5a668cc2ba7aa1003792fbbf3000a6ed69c0e53b1eeb2c1afc25cb" "b8720a6ec85bee63542f0b202763e0a40606863e9ca7ebd94b7fcd7744234312" "43b78a08f245bc198dadf35b324f445472c92dda3f1b6d1746cefee9f2ade177" "8d68cd652ab405df5db91acc66be58307e008bfac3ddc4beef7d7f814d37026c" "b87f0a7cc94fc07f1417f95de2382a7c1c853a6822d987af45b3b3c5e95e3abb" "df069ec238487ceab1cec64809a3c1dcef1393123ecdf430bdb7b94537ca2c6a" "7f954d5bee47a27cc6cc83d1d6b80f7a32d82f744a725375d6e22b65758f9a5e" "06a2eef27703cd3c8b017c90d9025d766ade307971826362c487a5273e14cc5a" "d6c5b14073abc649dad816750ef1ac7d5defdf1630d4e9938e06c38ad547d6da" "0ed28b0694dd2c7a2407598e63650a8562b9e833a1a136ee74790a74d3776d3b" "a37d20710ab581792b7c9f8a075fcbb775d4ffa6c8bce9137c84951b1b453016" "738c4838957c1884dfacbb6f4f783c54e87c4a6b31c336d6279fc1c2b2ee56c5" "2eb6dd78a83cdecc2ea783a2640f4c316f41e223353d4a32fe936113e8a2ddbf" "9040edb21d65cef8a4a4763944304c1a6655e85aabb6e164db6d5ba4fc494a04" "0ed3d96a506b89c1029a1ed904b11b5adcebeb2e0c16098c99c0ad95cb124729" "b6c43bb2aea78890cf6bd4a970e6e0277d2daf0075272817ea8bb53f9c6a7f0a" "aba75724c5d4d0ec0de949694bce5ce6416c132bb031d4e7ac1c4f2dbdd3d580" "67f0f440afa2e68d9d00219b5a56308761af45832fb60769d2b2fd36e3fead45" default))
+ '(display-line-numbers-type 'relative)
  '(evil-undo-system 'undo-tree)
  '(evil-want-keybinding nil)
  '(fill-column 80)
  '(global-company-mode t)
  '(ignored-local-variable-values
-   '((org-latex-hyperref-template)
+   '((org-tag-alist . classes)
+     (org-latex-hyperref-template)
      (org-latex-packages-alist)
      (org-latex-default-packages-alist)))
  '(ivy-posframe-height 20)
  '(ivy-posframe-mode t nil (ivy-posframe))
  '(ivy-posframe-width 120)
  '(org-agenda-files
-   '("/home/alkhaldieid/repos/csa/paper/paper.org" "/home/alkhaldieid/repos/org/agenda/monthly.org"))
+   '("/home/alkhaldieid/repos/org/agenda/salah.org" "/home/alkhaldieid/repos/writings/paper3/resubmit/resubmission/resubmission_plan.org" "/home/alkhaldieid/repos/csa/paper/paper.org" "/home/alkhaldieid/repos/org/agenda/monthly.org"))
  '(package-selected-packages
-   '(ivy-emms command-log-mode evil-surround twittering-mode visual-fill-column academic-phrases ivy-pass ytdl org-present evil-collections ivy-posframe git-gutter git-gutter-+ git-gutter-plus evil-nerd-commenter company-statistics company-quickhelp rainbow-mode evil-collection projectile hc-zenburn-theme danneskjold-theme zenburn-theme leuven-theme ample-theme conda flyspell-correct-ivy elpy lsp-mode jupyter major-mode-hydra pyvenv ob-ipython org-roam-bibtex deft company marginalia magit openwith olivetti org-superstar yasnippet try mw-thesaurus ivy-bibtex org-ref hydra avy htmlize citeproc evil-org bibtex-completion org-roam which-key use-package undo-tree eyebrowse evil-leader counsel))
+   '(orderless ef-themes ranger google-translate solaire-mode vscode-dark-plus-theme vterm ivy-emms command-log-mode evil-surround twittering-mode visual-fill-column academic-phrases ivy-pass ytdl org-present evil-collections ivy-posframe git-gutter git-gutter-+ git-gutter-plus evil-nerd-commenter company-statistics company-quickhelp rainbow-mode evil-collection projectile hc-zenburn-theme danneskjold-theme zenburn-theme leuven-theme ample-theme conda flyspell-correct-ivy elpy lsp-mode jupyter major-mode-hydra pyvenv ob-ipython org-roam-bibtex deft company marginalia magit openwith olivetti org-superstar yasnippet try mw-thesaurus ivy-bibtex org-ref hydra avy htmlize citeproc evil-org bibtex-completion org-roam which-key use-package undo-tree eyebrowse evil-leader counsel))
  '(smtpmail-smtp-server "email.utoledo.edu")
  '(smtpmail-smtp-service 25)
  '(visual-fill-column-center-text t))
@@ -32,4 +34,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 103 :width normal)))))
+ '(default ((t (:family "Source Code Variable" :foundry "ADBO" :slant normal :weight black :height 103 :width normal)))))
