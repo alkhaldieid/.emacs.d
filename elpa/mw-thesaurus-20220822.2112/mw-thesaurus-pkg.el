77699cac84899a28d53e755478a90f8d2e1d544e
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2023-07-08 06:26:49 -0400

    add elpa packages to the tracked files

---
+++ elpa/mw-thesaurus-20220822.2112/mw-thesaurus-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from mw-thesaurus.el  -*- no-byte-compile: t -*-
+(define-package "mw-thesaurus" "20220822.2112" "Merriam-Webster Thesaurus" '((emacs "25") (request "0.3.0") (dash "2.16.0")) :commit "bae48760b09f750359ef702875fdde783be2ce4d" :authors '(("Ag Ibragimov")) :maintainer '("Ag Ibragimov") :keywords '("wp" "matching") :url "https://github.com/agzam/mw-thesaurus.el")
