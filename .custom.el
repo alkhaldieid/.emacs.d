263a47e6a21962ba2858a1195b3c12f12036011b
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2023-07-17 22:29:06 +0300

    added support to vertico+orderless+embark+consult. Everything works fine

--- .custom.el
+++ .custom.el
@@ -8,7 +8,51 @@
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
  '(custom-safe-themes
-   '("578db0ce196a4c849aa995a0dd32d5fe85da59a1ec303614aa40a28bf9ad8b99" "83afe95f30785201eb9e189bf507b0f38076a436804c987c52830214ac025906" "eb93ba33f178940853271e32f889ad56e5cfece616cc14e776429cb02870bd26" "42f5c59e0b2aabac84a661586a939d8c459f658fae72887b46a8496389ef6b7e" "8d371c95370c5965746ccddd94fe96d7a2fbcdd67a77b3951db6fb11f68baf7d" "be5ebe527c28434cd4bdf33b0430a4eec557e59a63b632695a7ce7014c6713a7" "0f08efc35f1190204ac227e8c866b18400612d2137e2d13dcbf4693953681ff3" "aee6debe7b326de2968d8b023fdc9ee7e6c9996a80532186674f2e1376ad1782" "83f4659ff482028561aed742320eaa491b1416c7c43181647f0c8ef256610dca" "d16a286583f7326bb1e3baf7d83df3643533cfa9ac6f0601d1b4a595ad2db523" "bd82a9cabe0cb7edd0bd33f696e140ca331af235baab8b268fb2d098b0e366ce" "234c3805fb341b7ce2a9e8ce6d72dba9b81e9335422cfee838e128edfb8a9774" "a55c6f55eacfa36389dffc8672420b80454db33b59843a1923f3e3054a4643ca" "bfe046af7359f81d6bd4c47e09ecba8304940107752616124fbf405208b90c8f" "3c93094d214c034a89ed81a4bba720a23b090f38f7923a442c879c2bd4dcce5b" "d50da51d7dc41c7fc1ce9409a74fd7661540dc4bf54ef55148e59a7763318112" "73a183da135380d11ea344ccfe4e78dfe0a6ddcf09f59f050d373f7b4c397c19" "1070069a4671554a2c00b8375485896c1aa8144f5b8d99b86845069351770ee9" "b7bf7b95fb796614f0b36a7c4906a93db75ad0c6146f336c7ed085f6fceef93d" "d0a35d7f6d15d501525e2e134a7254096fc72ae42c0946458372bc7fd00a73ac" "47f3e55e4e3d570d5513d4cc58047dc059bd6ab3d135c796c4ccbfb77d4eb88b" "51e1ac68a9844a92b3402b30d3f9f568e15e2015812d4ee79819a489261f9d0a" "7343e856679eef5ad722f026037c92e5c60fc9eed6f38ef86b4170b3e524fee3" "159b4110d7b6b71531b3e70a27b773bc3cb13c66e3a5ce001cac064939e46887" "e8483bc21cf667971eabdbb01203e3a471de1b59a4e532368b7a20f5665d686a" "fb2c27e7ad4a81c2f57bceeca0faeb1c84fb7a167c097b998e2a1ea314455fd6" "92cfd42cedb42fdd3ea0d84d518825a94f29b30be20f65978dab7d7c8fa30c6a" "0f964c8dbc5a668cc2ba7aa1003792fbbf3000a6ed69c0e53b1eeb2c1afc25cb" "b8720a6ec85bee63542f0b202763e0a40606863e9ca7ebd94b7fcd7744234312" "43b78a08f245bc198dadf35b324f445472c92dda3f1b6d1746cefee9f2ade177" "8d68cd652ab405df5db91acc66be58307e008bfac3ddc4beef7d7f814d37026c" "b87f0a7cc94fc07f1417f95de2382a7c1c853a6822d987af45b3b3c5e95e3abb" "df069ec238487ceab1cec64809a3c1dcef1393123ecdf430bdb7b94537ca2c6a" "7f954d5bee47a27cc6cc83d1d6b80f7a32d82f744a725375d6e22b65758f9a5e" "06a2eef27703cd3c8b017c90d9025d766ade307971826362c487a5273e14cc5a" "d6c5b14073abc649dad816750ef1ac7d5defdf1630d4e9938e06c38ad547d6da" "0ed28b0694dd2c7a2407598e63650a8562b9e833a1a136ee74790a74d3776d3b" "a37d20710ab581792b7c9f8a075fcbb775d4ffa6c8bce9137c84951b1b453016" "738c4838957c1884dfacbb6f4f783c54e87c4a6b31c336d6279fc1c2b2ee56c5" "2eb6dd78a83cdecc2ea783a2640f4c316f41e223353d4a32fe936113e8a2ddbf" "9040edb21d65cef8a4a4763944304c1a6655e85aabb6e164db6d5ba4fc494a04" "0ed3d96a506b89c1029a1ed904b11b5adcebeb2e0c16098c99c0ad95cb124729" "b6c43bb2aea78890cf6bd4a970e6e0277d2daf0075272817ea8bb53f9c6a7f0a" "aba75724c5d4d0ec0de949694bce5ce6416c132bb031d4e7ac1c4f2dbdd3d580" "67f0f440afa2e68d9d00219b5a56308761af45832fb60769d2b2fd36e3fead45" default))
+   '("578db0ce196a4c849aa995a0dd32d5fe85da59a1ec303614aa40a28bf9ad8b99"
+     "83afe95f30785201eb9e189bf507b0f38076a436804c987c52830214ac025906"
+     "eb93ba33f178940853271e32f889ad56e5cfece616cc14e776429cb02870bd26"
+     "42f5c59e0b2aabac84a661586a939d8c459f658fae72887b46a8496389ef6b7e"
+     "8d371c95370c5965746ccddd94fe96d7a2fbcdd67a77b3951db6fb11f68baf7d"
+     "be5ebe527c28434cd4bdf33b0430a4eec557e59a63b632695a7ce7014c6713a7"
+     "0f08efc35f1190204ac227e8c866b18400612d2137e2d13dcbf4693953681ff3"
+     "aee6debe7b326de2968d8b023fdc9ee7e6c9996a80532186674f2e1376ad1782"
+     "83f4659ff482028561aed742320eaa491b1416c7c43181647f0c8ef256610dca"
+     "d16a286583f7326bb1e3baf7d83df3643533cfa9ac6f0601d1b4a595ad2db523"
+     "bd82a9cabe0cb7edd0bd33f696e140ca331af235baab8b268fb2d098b0e366ce"
+     "234c3805fb341b7ce2a9e8ce6d72dba9b81e9335422cfee838e128edfb8a9774"
+     "a55c6f55eacfa36389dffc8672420b80454db33b59843a1923f3e3054a4643ca"
+     "bfe046af7359f81d6bd4c47e09ecba8304940107752616124fbf405208b90c8f"
+     "3c93094d214c034a89ed81a4bba720a23b090f38f7923a442c879c2bd4dcce5b"
+     "d50da51d7dc41c7fc1ce9409a74fd7661540dc4bf54ef55148e59a7763318112"
+     "73a183da135380d11ea344ccfe4e78dfe0a6ddcf09f59f050d373f7b4c397c19"
+     "1070069a4671554a2c00b8375485896c1aa8144f5b8d99b86845069351770ee9"
+     "b7bf7b95fb796614f0b36a7c4906a93db75ad0c6146f336c7ed085f6fceef93d"
+     "d0a35d7f6d15d501525e2e134a7254096fc72ae42c0946458372bc7fd00a73ac"
+     "47f3e55e4e3d570d5513d4cc58047dc059bd6ab3d135c796c4ccbfb77d4eb88b"
+     "51e1ac68a9844a92b3402b30d3f9f568e15e2015812d4ee79819a489261f9d0a"
+     "7343e856679eef5ad722f026037c92e5c60fc9eed6f38ef86b4170b3e524fee3"
+     "159b4110d7b6b71531b3e70a27b773bc3cb13c66e3a5ce001cac064939e46887"
+     "e8483bc21cf667971eabdbb01203e3a471de1b59a4e532368b7a20f5665d686a"
+     "fb2c27e7ad4a81c2f57bceeca0faeb1c84fb7a167c097b998e2a1ea314455fd6"
+     "92cfd42cedb42fdd3ea0d84d518825a94f29b30be20f65978dab7d7c8fa30c6a"
+     "0f964c8dbc5a668cc2ba7aa1003792fbbf3000a6ed69c0e53b1eeb2c1afc25cb"
+     "b8720a6ec85bee63542f0b202763e0a40606863e9ca7ebd94b7fcd7744234312"
+     "43b78a08f245bc198dadf35b324f445472c92dda3f1b6d1746cefee9f2ade177"
+     "8d68cd652ab405df5db91acc66be58307e008bfac3ddc4beef7d7f814d37026c"
+     "b87f0a7cc94fc07f1417f95de2382a7c1c853a6822d987af45b3b3c5e95e3abb"
+     "df069ec238487ceab1cec64809a3c1dcef1393123ecdf430bdb7b94537ca2c6a"
+     "7f954d5bee47a27cc6cc83d1d6b80f7a32d82f744a725375d6e22b65758f9a5e"
+     "06a2eef27703cd3c8b017c90d9025d766ade307971826362c487a5273e14cc5a"
+     "d6c5b14073abc649dad816750ef1ac7d5defdf1630d4e9938e06c38ad547d6da"
+     "0ed28b0694dd2c7a2407598e63650a8562b9e833a1a136ee74790a74d3776d3b"
+     "a37d20710ab581792b7c9f8a075fcbb775d4ffa6c8bce9137c84951b1b453016"
+     "738c4838957c1884dfacbb6f4f783c54e87c4a6b31c336d6279fc1c2b2ee56c5"
+     "2eb6dd78a83cdecc2ea783a2640f4c316f41e223353d4a32fe936113e8a2ddbf"
+     "9040edb21d65cef8a4a4763944304c1a6655e85aabb6e164db6d5ba4fc494a04"
+     "0ed3d96a506b89c1029a1ed904b11b5adcebeb2e0c16098c99c0ad95cb124729"
+     "b6c43bb2aea78890cf6bd4a970e6e0277d2daf0075272817ea8bb53f9c6a7f0a"
+     "aba75724c5d4d0ec0de949694bce5ce6416c132bb031d4e7ac1c4f2dbdd3d580"
+     "67f0f440afa2e68d9d00219b5a56308761af45832fb60769d2b2fd36e3fead45" default))
  '(display-line-numbers-type 'relative)
  '(evil-undo-system 'undo-tree)
  '(evil-want-keybinding nil)
@@ -17,24 +61,35 @@
  '(global-display-line-numbers-mode t)
  '(helm-minibuffer-history-key "M-p")
  '(ignored-local-variable-values
-   '((org-tag-alist . classes)
-     (org-latex-hyperref-template)
-     (org-latex-packages-alist)
-     (org-latex-default-packages-alist)))
+   '((org-tag-alist . classes) (org-latex-hyperref-template)
+     (org-latex-packages-alist) (org-latex-default-packages-alist)))
  '(ivy-posframe-height 20)
  '(ivy-posframe-min-height 25)
  '(ivy-posframe-mode t nil (ivy-posframe))
  '(ivy-posframe-width 120)
  '(org-agenda-files
-   '("/home/alkhaldieid/repos/org/agenda/salah.org" "/home/alkhaldieid/repos/writings/paper3/resubmit/resubmission/resubmission_plan.org" "/home/alkhaldieid/repos/csa/paper/paper.org" "/home/alkhaldieid/repos/org/agenda/monthly.org"))
+   '("~/repos/org/agenda/jobs.org"
+     "/home/alkhaldieid/repos/org/roam/daily/2023-07-16.org"))
  '(org-file-apps
-   '((auto-mode . emacs)
-     (directory . emacs)
-     ("\\.mm\\'" . default)
-     ("\\.x?html?\\'" . default)
-     ("\\.pdf\\'" . system)))
+   '((auto-mode . emacs) (directory . emacs) ("\\.mm\\'" . default)
+     ("\\.x?html?\\'" . default) ("\\.pdf\\'" . system)))
  '(package-selected-packages
-   '(ivy-hydra ivy-hedra fzf openwith helm latex-preview-pane org-pomodoro smartparens orderless ef-themes ranger google-translate solaire-mode vscode-dark-plus-theme vterm ivy-emms command-log-mode evil-surround twittering-mode visual-fill-column academic-phrases ivy-pass ytdl org-present evil-collections ivy-posframe git-gutter git-gutter-+ git-gutter-plus evil-nerd-commenter company-statistics company-quickhelp rainbow-mode evil-collection projectile hc-zenburn-theme danneskjold-theme zenburn-theme leuven-theme ample-theme conda flyspell-correct-ivy elpy lsp-mode jupyter major-mode-hydra pyvenv ob-ipython org-roam-bibtex deft company marginalia magit olivetti org-superstar yasnippet try mw-thesaurus ivy-bibtex org-ref hydra avy htmlize citeproc evil-org bibtex-completion org-roam which-key use-package undo-tree eyebrowse evil-leader counsel))
+   '(embark-consult embark consult vertico ivy-hydra ivy-hedra fzf openwith helm
+		    latex-preview-pane org-pomodoro smartparens orderless
+		    ef-themes ranger google-translate solaire-mode
+		    vscode-dark-plus-theme vterm ivy-emms command-log-mode
+		    evil-surround twittering-mode visual-fill-column
+		    academic-phrases ivy-pass ytdl org-present evil-collections
+		    ivy-posframe git-gutter git-gutter-+ git-gutter-plus
+		    evil-nerd-commenter company-statistics company-quickhelp
+		    rainbow-mode evil-collection projectile hc-zenburn-theme
+		    danneskjold-theme zenburn-theme leuven-theme ample-theme
+		    conda flyspell-correct-ivy elpy lsp-mode jupyter
+		    major-mode-hydra pyvenv ob-ipython org-roam-bibtex deft
+		    company marginalia magit olivetti org-superstar yasnippet
+		    try mw-thesaurus ivy-bibtex org-ref hydra avy htmlize
+		    citeproc evil-org bibtex-completion org-roam which-key
+		    use-package undo-tree eyebrowse evil-leader counsel))
  '(smtpmail-smtp-server "email.utoledo.edu")
  '(smtpmail-smtp-service 25)
  '(tool-bar-mode nil)
